626da887b927ed78bd5e2b46238a67abda0834d0
임우택 <lyt1228@naver.com>
2023-09-04 10:05:49 +0900

    feat: setup newkids server project

---
+++ server/src/main/java/com/ssafy/newkids/NewkidsApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.newkids;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewkidsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewkidsApplication.class, args);
+	}
+
+}
