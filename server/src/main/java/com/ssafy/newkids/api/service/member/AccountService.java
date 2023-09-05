c92312a249c68f5df456cdb6cfd44d77a0721763
임우택 <lyt1228@naver.com>
2023-09-05 17:25:46 +0900

    feat: 닉네임 중복 체크 서비스 스켈레톤 구현
    - AccountService#checkNickname
    
    Adds S09P22C205-65

--- server/src/main/java/com/ssafy/newkids/api/service/member/AccountService.java
+++ server/src/main/java/com/ssafy/newkids/api/service/member/AccountService.java
@@ -40,4 +40,8 @@ public class AccountService {
     public Boolean checkEmail(String email) {
         return null;
     }
+
+    public Boolean checkNickname(String nickname) {
+        return null;
+    }
 }
