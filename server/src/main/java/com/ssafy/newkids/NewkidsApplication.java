31d95063db154ec2745ca8f615338a168cf85835
Chaos0103 <lyt1228@naver.com>
2023-09-06 00:52:38 +0900

    feat: BCryptPasswordEncoder 빈 등록
    Adds S09P22C205-83

--- server/src/main/java/com/ssafy/newkids/NewkidsApplication.java
+++ server/src/main/java/com/ssafy/newkids/NewkidsApplication.java
@@ -2,12 +2,18 @@ package com.ssafy.newkids;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class NewkidsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NewkidsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(NewkidsApplication.class, args);
+    }
 
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
