e9a8d9f3c93dcbae5ec9950475f29dd86fe362d8
임우택 <lyt1228@naver.com>
2023-09-06 11:14:58 +0900

    feat: 닉네임 중복 체크 서비스 구현
    - AccountService#checkNickname
    
    Adds S09P22C205-100

--- server/src/main/java/com/ssafy/newkids/api/service/member/AccountService.java
+++ server/src/main/java/com/ssafy/newkids/api/service/member/AccountService.java
@@ -63,8 +63,14 @@ public class AccountService implements UserDetailsService {
         return memberQueryRepository.existEmail(email);
     }
 
-    public Boolean checkNickname(String nickname) {
-        return null;
+    /**
+     * 닉네임 중복 체크
+     *
+     * @param nickname 중복 체크할 대상 닉네임
+     * @return 존재하면 true, 존재하지 않으면 false
+     */
+    public boolean checkNickname(String nickname) {
+        return memberQueryRepository.existNickname(nickname);
     }
 
     @Override
