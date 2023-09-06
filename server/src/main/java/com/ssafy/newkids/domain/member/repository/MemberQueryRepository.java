450c27fb6c3c62415131cb496f4f876add698a1e
임우택 <lyt1228@naver.com>
2023-09-06 10:04:44 +0900

    feat: 닉네임 중복 체크 쿼리 구현
    - MemberQueryRepository#existNickname
    
    Adds S09P22C205-85

--- server/src/main/java/com/ssafy/newkids/domain/member/repository/MemberQueryRepository.java
+++ server/src/main/java/com/ssafy/newkids/domain/member/repository/MemberQueryRepository.java
@@ -36,4 +36,20 @@ public class MemberQueryRepository {
 
         return result != null;
     }
+
+    /**
+     * 닉네임 중복 체크
+     *
+     * @param nickname 중복 체크할 대상 닉네임
+     * @return 존재하면 true, 존재하지 않으면 false
+     */
+    public boolean existNickname(String nickname) {
+        Integer result = queryFactory
+            .selectOne()
+            .from(member)
+            .where(member.nickname.eq(nickname))
+            .fetchFirst();
+
+        return result != null;
+    }
 }
