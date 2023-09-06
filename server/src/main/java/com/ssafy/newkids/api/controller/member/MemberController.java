7e799ff2e2ebb2bb3832a243ff5f06113c5aa5ab
임우택 <lyt1228@naver.com>
2023-09-06 14:07:44 +0900

    feat: 비밀번호 변경 컨트롤러 구현
    - MemberController#editPassword
    
    Adds S09P22C205-39

--- server/src/main/java/com/ssafy/newkids/api/controller/member/MemberController.java
+++ server/src/main/java/com/ssafy/newkids/api/controller/member/MemberController.java
@@ -1,6 +1,7 @@
 package com.ssafy.newkids.api.controller.member;
 
 import com.ssafy.newkids.api.ApiResponse;
+import com.ssafy.newkids.api.controller.member.request.EditPasswordRequest;
 import com.ssafy.newkids.api.controller.member.request.JoinRequest;
 import com.ssafy.newkids.api.controller.member.request.WithdrawalRequest;
 import com.ssafy.newkids.api.controller.member.response.JoinMemberResponse;
@@ -8,10 +9,7 @@ import com.ssafy.newkids.api.controller.member.response.MemberResponse;
 import com.ssafy.newkids.api.service.member.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -34,6 +32,27 @@ public class MemberController {
 
         JoinMemberResponse response = memberService.join(request.toJoinMemberDto());
         log.debug("JoinMemberResponse={}", response);
+
+        return ApiResponse.ok(response);
+    }
+
+    /**
+     * 계정 비밀번호 변경 API
+     *
+     * @param request 변경할 계정 비밀번호 정보
+     * @return 비밀번호 변경된 계정 정보
+     */
+    @PatchMapping("/password")
+    public ApiResponse<MemberResponse> editPassword(@Valid @RequestBody EditPasswordRequest request) {
+        log.debug("call MemberController#editPassword");
+        log.debug("EditPasswordRequest={}", request);
+
+        // TODO: 2023-09-05 임우택 JWT에서 회원 정보 추출
+        String email = "ssafy@ssafy.com";
+        log.info("access email={}", email);
+
+        MemberResponse response = memberService.editPassword(email, request.getCurrentPwd(), request.getNewPwd());
+        log.debug("MemberResponse={}", response);
 
         return ApiResponse.ok(response);
     }
