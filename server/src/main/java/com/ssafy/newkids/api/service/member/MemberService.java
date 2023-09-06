886cd88f217aea3a4cd949e3df56390b8a859795
임우택 <lyt1228@naver.com>
2023-09-06 10:17:56 +0900

    refactor: 회원 가입 서비스 코드 정리
    - MemberService#join
    - MemberService#duplicationCheckByEmail
    - MemberService#duplicationCheckByNickname
    - MemberService#createMember
    
    Adds S09P22C205-85

--- server/src/main/java/com/ssafy/newkids/api/service/member/MemberService.java
+++ server/src/main/java/com/ssafy/newkids/api/service/member/MemberService.java
@@ -25,28 +25,62 @@ public class MemberService {
     private final MemberQueryRepository memberQueryRepository;
     private final BCryptPasswordEncoder passwordEncoder;
 
+    /**
+     * 회원 가입
+     *
+     * @param dto 회원의 정보
+     * @return 가입을 성공한 회원의 기본 정보
+     * @throws DuplicateException 이미 사용중인 이메일, 닉네임인 경우
+     */
     public JoinMemberResponse join(JoinMemberDto dto) {
-        //이메일 중복 체크
-        boolean checkEmail = memberQueryRepository.existEmail(dto.getEmail());
-        if (checkEmail) {
-            throw new DuplicateException("이미 사용중인 이메일입니다.");
-        }
+        duplicationCheckByEmail(dto.getEmail());
+        duplicationCheckByNickname(dto.getNickname());
 
-        boolean checkNickname = memberQueryRepository.existNickname(dto.getNickname());
-        if (checkNickname) {
-            throw new DuplicateException("이미 사용중인 닉네임입니다.");
-        }
-
-        //닉네임 중복 체크
-        String encryptedPwd = passwordEncoder.encode(dto.getPassword());
-
-        Member member = dto.toEntity(encryptedPwd);
-        Member savedMember = memberRepository.save(member);
+        Member savedMember = createMember(dto);
 
         return JoinMemberResponse.of(savedMember);
     }
 
     public Boolean withdrawal(String email, String password) {
         return null;
+    }
+
+    /**
+     * 이메일 중복 체크
+     *
+     * @param email 중복 체크할 이메일
+     * @throws DuplicateException 이미 사용중인 이메일인 경우
+     */
+    private void duplicationCheckByEmail(String email) {
+        boolean checkEmail = memberQueryRepository.existEmail(email);
+        if (checkEmail) {
+            throw new DuplicateException("이미 사용중인 이메일입니다.");
+        }
+    }
+
+    /**
+     * 닉네임 중복 체크
+     *
+     * @param nickname 중복 체크할 닉네임
+     * @throws DuplicateException 이미 사용중인 닉네임인 경우
+     */
+    private void duplicationCheckByNickname(String nickname) {
+        boolean checkNickname = memberQueryRepository.existNickname(nickname);
+        if (checkNickname) {
+            throw new DuplicateException("이미 사용중인 닉네임입니다.");
+        }
+    }
+
+    /**
+     * 회원 엔티티 생성 및 DB 저장
+     *
+     * @param dto 회원 정보
+     * @return 가입된 회원 엔티티
+     */
+    private Member createMember(JoinMemberDto dto) {
+        String encryptedPwd = passwordEncoder.encode(dto.getPassword());
+
+        Member member = dto.toEntity(encryptedPwd);
+        return memberRepository.save(member);
     }
 }
