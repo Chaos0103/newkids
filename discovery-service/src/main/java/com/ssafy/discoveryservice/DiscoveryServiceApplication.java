0e61181863008baa389a80956b605943873c3edc
임우택 <lyt1228@naver.com>
2023-09-07 16:00:57 +0900

    build: apigateway-service 환경설정
    Closes S09P22C205-155

--- discovery-service/src/main/java/com/ssafy/discoveryservice/DiscoveryServiceApplication.java
+++ discovery-service/src/main/java/com/ssafy/discoveryservice/DiscoveryServiceApplication.java
@@ -1,8 +1,11 @@
 package com.ssafy.discoveryservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaServer
@@ -10,6 +13,11 @@ public class DiscoveryServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DiscoveryServiceApplication.class, args);
+	}
+
+	@Bean
+	public HttpTraceRepository httpTraceRepository() {
+		return new InMemoryHttpTraceRepository();
 	}
 
 }
