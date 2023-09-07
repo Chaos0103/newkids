a77a1465e1b783c180a58b46c8f7f123978c1467
임우택 <lyt1228@naver.com>
2023-09-07 12:04:25 +0900

    feat: 단어 컨트롤러 스켈레톤 코드 생성
    - WordController#createWord
    - WordController#getWords
    - WordController#editWord
    - WordController#removeWord
    
    Adds S09P22C205-65

--- server/src/main/java/com/ssafy/newkids/api/controller/vocabulary/VocabularyController.java
+++ server/src/main/java/com/ssafy/newkids/api/controller/vocabulary/VocabularyController.java
@@ -49,7 +49,7 @@ public class VocabularyController {
         return ApiResponse.found(response);
     }
 
-    //    D: 단어장 삭제
+    // TODO: 2023-09-07 임우택 단어장 삭제 API
     @DeleteMapping("/{vocabularyId}")
     @ResponseStatus(HttpStatus.FOUND)
     public ApiResponse<VocabularyResponse> removeVocabulary(@PathVariable Long vocabularyId) {
