48e2dfc8562a942ead08b381794b272b6796c5fd
임우택 <lyt1228@naver.com>
2023-09-07 12:03:34 +0900

    feat: 단어 명령 서비스, 단어 쿼리 서비스 스켈레톤 코드 생성
    - WordService#createWord
    - WordService#editWord
    - WordService#removeWord
    - WordQueryService#getWords
    
    Adds S09P22C205-65

--- server/src/main/java/com/ssafy/newkids/api/service/vocabulary/dto/CreateWordDto.java
+++ server/src/main/java/com/ssafy/newkids/api/service/vocabulary/dto/CreateWordDto.java
@@ -4,14 +4,14 @@ import lombok.Builder;
 import lombok.Data;
 
 @Data
-public class CreateVocabularyDto {
+public class CreateWordDto {
 
     private String wordKey;
     private String word;
     private String description;
 
     @Builder
-    private CreateVocabularyDto(String wordKey, String word, String description) {
+    private CreateWordDto(String wordKey, String word, String description) {
         this.wordKey = wordKey;
         this.word = word;
         this.description = description;
