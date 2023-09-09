c135885ca6cb5607be885f30f2b4f7eb13a6df2e
Chaos0103 <lyt1228@naver.com>
2023-09-09 13:39:29 +0900

    feat(vocabulary-service): 단어 등록 서비스 구현
    Closes S09P22C205-159

--- vocabulary-service/src/main/java/com/ssafy/vocabularyservice/api/service/word/WordService.java
+++ vocabulary-service/src/main/java/com/ssafy/vocabularyservice/api/service/word/WordService.java
@@ -3,6 +3,10 @@ package com.ssafy.vocabularyservice.api.service.word;
 import com.ssafy.vocabularyservice.api.controller.word.response.WordResponse;
 import com.ssafy.vocabularyservice.api.service.word.dto.CreateWordDto;
 import com.ssafy.vocabularyservice.api.service.word.dto.EditWordDto;
+import com.ssafy.vocabularyservice.api.service.word.exception.DuplicateException;
+import com.ssafy.vocabularyservice.domain.word.Word;
+import com.ssafy.vocabularyservice.domain.word.repository.WordQueryRepository;
+import com.ssafy.vocabularyservice.domain.word.repository.WordRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,8 +16,19 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class WordService {
 
+    private final WordRepository wordRepository;
+    private final WordQueryRepository wordQueryRepository;
+
     public WordResponse createWord(CreateWordDto dto) {
-        return null;
+        boolean isExistWordKey = wordQueryRepository.existWordKey(dto.getWordKey());
+        if (isExistWordKey) {
+            throw new DuplicateException("이미 사용중인 단어키입니다.");
+        }
+
+        Word word = dto.toEntity();
+        Word savedWord = wordRepository.save(word);
+
+        return WordResponse.of(savedWord);
     }
 
     public WordResponse editWord(String wordId, EditWordDto dto) {
--- vocabulary-service/src/main/java/com/ssafy/vocabularyservice/api/service/word/dto/CreateWordDto.java
+++ vocabulary-service/src/main/java/com/ssafy/vocabularyservice/api/service/word/dto/CreateWordDto.java
@@ -1,5 +1,6 @@
 package com.ssafy.vocabularyservice.api.service.word.dto;
 
+import com.ssafy.vocabularyservice.domain.word.Word;
 import lombok.Builder;
 import lombok.Data;
 
@@ -15,5 +16,13 @@ public class CreateWordDto {
         this.wordKey = wordKey;
         this.content = content;
         this.description = description;
+    }
+
+    public Word toEntity() {
+        return Word.builder()
+            .wordKey(this.wordKey)
+            .content(this.content)
+            .description(this.description)
+            .build();
     }
 }
