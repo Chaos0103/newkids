c64c03a72b4b102940ea3ce1bcebf5b412b41b0b
wjs_5025@naver.com <wjs_5025@naver.com>
2023-09-11 16:37:19 +0900

    feat: navigation-menubar (Menubar, MenubarItem)

--- client/src/components/organisms/common/Menubar/index.tsx
+++ client/src/components/organisms/common/Menubar/index.tsx
@@ -1,12 +1,16 @@
-import React, { useEffect } from 'react';
+import React from 'react';
+import MenubarItem from 'components/atoms/common/MenubarItem';
+import { Menus } from 'constants/common';
 import { MenubarContainer } from './style';
 
 function Menubar() {
-	useEffect(() => {
-		return () => {};
-	}, []);
-
-	return <MenubarContainer>Menubar</MenubarContainer>;
+	return (
+		<MenubarContainer>
+			{Menus.map((el) => (
+				<MenubarItem key={el.key} menu={el} />
+			))}
+		</MenubarContainer>
+	);
 }
 
 export default Menubar;
