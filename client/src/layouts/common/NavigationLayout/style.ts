c29bca3979932f26cf31d8373514f4c973071876
wjs_5025@naver.com <wjs_5025@naver.com>
2023-09-11 16:37:35 +0900

    design: add box-shadow

--- client/src/layouts/common/NavigationLayout/style.ts
+++ client/src/layouts/common/NavigationLayout/style.ts
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 export const NavigationLayoutWrapper = styled.div`
+	box-shadow: 0 3px 30px 0 rgba(0, 0, 0, 0.03);
 	position: fixed;
 	width: 100%;
 	background-color: var(--white-color);
