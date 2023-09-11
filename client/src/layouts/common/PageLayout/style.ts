3be08582ea75df8454e52119c8f7e5e43bcf20e3
wjs_5025@naver.com <wjs_5025@naver.com>
2023-09-11 11:37:38 +0900

    fix: PageLayout max height

--- client/src/layouts/common/PageLayout/style.ts
+++ client/src/layouts/common/PageLayout/style.ts
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 export const PageLayoutContainer = styled.div`
 	position: relative;
 	min-height: 100vh;
-	max-height: 100vh;
 	padding-top: 166px;
 	background-color: #f1f1f1;
 `;
