c5f637bbeacf62d328a3596bf42d3f7edcfea5d6
임우택 <lyt1228@naver.com>
2023-09-11 11:29:16 +0900

    test(vocabulary-service): 단어장 등록 API 컨트롤러 테스트 구현
    Closes S09P22C205-173

--- vocabulary-service/src/main/java/com/ssafy/vocabularyservice/api/controller/vocabulary/VocabularyController.java
+++ vocabulary-service/src/main/java/com/ssafy/vocabularyservice/api/controller/vocabulary/VocabularyController.java
@@ -1,4 +1,31 @@
 package com.ssafy.vocabularyservice.api.controller.vocabulary;
 
+import com.ssafy.vocabularyservice.api.controller.ApiResponse;
+import com.ssafy.vocabularyservice.api.controller.vocabulary.request.CreateVocabularyRequest;
+import com.ssafy.vocabularyservice.api.controller.vocabulary.response.WordResponse;
+import com.ssafy.vocabularyservice.api.service.vocabulary.VocabularyService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+/**
+ * 단어장 API 컨트롤러
+ * @author 임우택
+ */
+@RequiredArgsConstructor
+@RestController
+@Slf4j
+@RequestMapping("/vocabulary-service")
 public class VocabularyController {
+
+    private final VocabularyService vocabularyService;
+
+    @PostMapping("/{memberKey}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ApiResponse<WordResponse> createVocabulary(@Valid @RequestBody CreateVocabularyRequest request, @PathVariable String memberKey) {
+        return ApiResponse.created(null);
+    }
 }
--- vocabulary-service/src/test/java/com/ssafy/vocabularyservice/ControllerTestSupport.java
+++ vocabulary-service/src/test/java/com/ssafy/vocabularyservice/ControllerTestSupport.java
@@ -1,11 +1,13 @@
 package com.ssafy.vocabularyservice;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ssafy.vocabularyservice.api.controller.vocabulary.VocabularyController;
+import com.ssafy.vocabularyservice.api.controller.word.WordController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest
+//@WebMvcTest(controllers = {WordController.class, VocabularyController.class})
 public abstract class ControllerTestSupport {
 
     @Autowired
