3bb652ff4ea4b1ed2899abe9e64c7aee525d253a
wjs_5025@naver.com <wjs_5025@naver.com>
2023-09-11 11:55:07 +0900

    fix: Button disbled optional 처리

--- client/src/components/atoms/common/Button/index.tsx
+++ client/src/components/atoms/common/Button/index.tsx
@@ -7,14 +7,14 @@ interface IButtonProps {
 	color: 'Normal' | 'Primary' | 'Danger' | 'Success' | 'SubFirst' | 'SubSecond';
 	text: string;
 	handleClick: () => void;
-	disabled: boolean;
+	disabled?: boolean;
 }
 
 function Button(props: IButtonProps) {
 	const { size, radius, color, text, handleClick, disabled } = props;
 
 	return (
-		<ButtonWrapper $size={size} $radius={radius} $color={color} onClick={handleClick} disabled={disabled}>
+		<ButtonWrapper $size={size} $radius={radius} $color={color} onClick={handleClick} disabled={disabled ?? false}>
 			{text}
 		</ButtonWrapper>
 	);
