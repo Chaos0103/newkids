b63e1d047535880685bb7b880c5d7360e5db9500
임우택 <lyt1228@naver.com>
2023-09-11 11:10:12 +0900

    feat(vocabulary-service): 단어장 등록 서비스 구현
    Closes S09P22C205-173

--- vocabulary-service/src/main/java/com/ssafy/vocabularyservice/api/service/vocabulary/VocabularyService.java
+++ vocabulary-service/src/main/java/com/ssafy/vocabularyservice/api/service/vocabulary/VocabularyService.java
@@ -1,11 +1,18 @@
 package com.ssafy.vocabularyservice.api.service.vocabulary;
 
 import com.ssafy.vocabularyservice.api.controller.vocabulary.response.WordResponse;
+import com.ssafy.vocabularyservice.api.service.vocabulary.exception.DuplicateException;
+import com.ssafy.vocabularyservice.domain.vocabulary.Vocabulary;
+import com.ssafy.vocabularyservice.domain.vocabulary.repository.VocabularyQueryRepository;
 import com.ssafy.vocabularyservice.domain.vocabulary.repository.VocabularyRepository;
+import com.ssafy.vocabularyservice.domain.word.Word;
 import com.ssafy.vocabularyservice.domain.word.repository.WordRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 /**
  * 단어장 명령 서비스
@@ -17,9 +24,31 @@ import org.springframework.transaction.annotation.Transactional;
 public class VocabularyService {
 
     private final VocabularyRepository vocabularyRepository;
+    private final VocabularyQueryRepository vocabularyQueryRepository;
     private final WordRepository wordRepository;
 
     public WordResponse createVocabulary(String memberKey, String workKey) {
-        return null;
+        //중복 검사
+        boolean isExistVocabulary = vocabularyQueryRepository.existVocabulary(memberKey, workKey);
+        if (isExistVocabulary) {
+            throw new DuplicateException("이미 단어장에 등록된 단어입니다.");
+        }
+
+        //단어 조회
+        Optional<Word> findWord = wordRepository.findByWordKey(workKey);
+        if (findWord.isEmpty()) {
+            throw new NoSuchElementException("등록되지 않은 단어입니다.");
+        }
+        Word word = findWord.get();
+
+        Vocabulary vocabulary = Vocabulary.builder()
+            .check(false)
+            .memberKey(memberKey)
+            .word(word)
+            .build();
+        Vocabulary savedVocabulary = vocabularyRepository.save(vocabulary);
+
+        //단어장 저장
+        return WordResponse.of(word);
     }
 }
