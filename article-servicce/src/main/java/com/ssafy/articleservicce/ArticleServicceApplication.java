ba1c1e4daf75132fbdb33a865b831dc71e2ab1d7
Chaos0103 <lyt1228@naver.com>
2023-09-13 00:49:00 +0900

    feat(article-service): setup article service project
    Closes S09P22C205-189

---
+++ article-servicce/src/main/java/com/ssafy/articleservicce/ArticleServicceApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.articleservicce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticleServicceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArticleServicceApplication.class, args);
+	}
+
+}
