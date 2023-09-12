78e4abf8fd6a57cd83ebe9777ec642cd6f984826
Chaos0103 <lyt1228@naver.com>
2023-09-13 01:00:09 +0900

    feat(article-service): 마이크로 서비스 등록
    Closes S09P22C205-189

--- article-servicce/src/main/java/com/ssafy/articleservicce/ArticleServicceApplication.java
+++ article-servicce/src/main/java/com/ssafy/articleservicce/ArticleServicceApplication.java
@@ -2,8 +2,10 @@ package com.ssafy.articleservicce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ArticleServicceApplication {
 
 	public static void main(String[] args) {
