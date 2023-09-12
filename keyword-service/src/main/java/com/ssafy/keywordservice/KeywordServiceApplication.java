b5d2c18ebf391c4300aff98ec2807380d6c7f148
Chaos0103 <lyt1228@naver.com>
2023-09-13 02:26:56 +0900

    feat(keyword-service): setup keyword service project
    Closes S09P22C205-189

---
+++ keyword-service/src/main/java/com/ssafy/keywordservice/KeywordServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.keywordservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeywordServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeywordServiceApplication.class, args);
+	}
+
+}
