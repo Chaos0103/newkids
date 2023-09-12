c5f0b9098b8f2773bed58d3dfbfd959f7198067d
jochanik <whcksdlr5909@naver.com>
2023-09-12 17:55:51 +0900

    feat: 주간 단어 퀴즈 메인화면 구현

---
+++ client/src/components/atoms/quiz/Title/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { TitleWrapper } from './style';
+
+function Title() {
+	return (
+		<TitleWrapper>
+			<b className="main-title">주간 단어 퀴즈</b>
+		</TitleWrapper>
+	);
+}
+
+export default Title;
