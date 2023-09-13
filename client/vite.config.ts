d8a9584f9fca558b070c114775507d0931c93cca
wjs_5025@naver.com <wjs_5025@naver.com>
2023-09-13 17:51:29 +0900

    build: hide serverURL (.env)

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,22 +1,26 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import svgr from 'vite-plugin-svgr';
 
 // TODO : froxy 설정 시, url 숨기기(.env)
-export default defineConfig({
-	plugins: [react(), eslint(), tsconfigPaths(), svgr()],
-	base: '/index',
-	server: {
-		proxy: {
-			'/api': {
-				target: 'https://j9c205.p.ssafy.io',
-				changeOrigin: true,
-				rewrite: (path) => path.replace(/%\/api/, ''),
-				secure: false,
-				ws: true,
+export default defineConfig(async ({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), '');
+
+	return {
+		plugins: [react(), eslint(), tsconfigPaths(), svgr()],
+		base: '/index',
+		server: {
+			proxy: {
+				'/api': {
+					target: env.VITE_APP_SERVER_BASE_URL,
+					changeOrigin: true,
+					rewrite: (path) => path.replace(/%\/api/, ''),
+					secure: false,
+					ws: true,
+				},
 			},
 		},
-	},
+	};
 });
