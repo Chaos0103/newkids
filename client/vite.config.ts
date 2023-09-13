85cb094c83949ea75451b93eab55e0152a4745e5
wjs_5025@naver.com <wjs_5025@naver.com>
2023-09-13 17:44:50 +0900

    build: add froxy

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,8 +4,19 @@ import eslint from 'vite-plugin-eslint';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import svgr from 'vite-plugin-svgr';
 
-// https://vitejs.dev/config/
+// TODO : froxy 설정 시, url 숨기기(.env)
 export default defineConfig({
 	plugins: [react(), eslint(), tsconfigPaths(), svgr()],
 	base: '/index',
+	server: {
+		proxy: {
+			'/api': {
+				target: 'https://j9c205.p.ssafy.io',
+				changeOrigin: true,
+				rewrite: (path) => path.replace(/%\/api/, ''),
+				secure: false,
+				ws: true,
+			},
+		},
+	},
 });
