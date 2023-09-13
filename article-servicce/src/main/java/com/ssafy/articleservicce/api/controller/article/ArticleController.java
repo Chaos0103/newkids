0ea61c24cf07d4a52772f480e68e8e094e945554
임우택 <lyt1228@naver.com>
2023-09-13 17:17:33 +0900

    refactor(article-service): 불필요 컨트롤러 삭제
    Closes S09P22C205-189

--- article-servicce/src/main/java/com/ssafy/articleservicce/api/controller/article/ArticleController.java
+++ article-servicce/src/main/java/com/ssafy/articleservicce/api/controller/article/ArticleController.java
@@ -1,7 +1,6 @@
 package com.ssafy.articleservicce.api.controller.article;
 
 import com.ssafy.articleservicce.api.controller.ApiResponse;
-import com.ssafy.articleservicce.api.controller.article.request.CreateArticleRequest;
 import com.ssafy.articleservicce.api.controller.article.response.ArticleDetailResponse;
 import com.ssafy.articleservicce.api.controller.article.response.ArticleResponse;
 import com.ssafy.articleservicce.api.service.article.ArticleQueryService;
@@ -11,8 +10,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
-
 @RequiredArgsConstructor
 @RestController
 @Slf4j
@@ -21,12 +18,6 @@ public class ArticleController {
 
     private final ArticleService articleService;
     private final ArticleQueryService articleQueryService;
-
-    // TODO: 2023/09/13 뉴스 기사 등록 API
-    @PostMapping
-    public ApiResponse<ArticleResponse> createArticle(@Valid @RequestBody CreateArticleRequest request) {
-        return ApiResponse.created(null);
-    }
 
     // TODO: 2023/09/13 뉴스 기사 조회 API
     @GetMapping
@@ -38,12 +29,6 @@ public class ArticleController {
     @GetMapping("/{articleId}")
     public ApiResponse<ArticleDetailResponse> getArticle(@PathVariable Long articleId) {
         return ApiResponse.ok(null);
-    }
-
-    // TODO: 2023/09/13 뉴스 기사 수정 API
-    @PatchMapping("/{articleId}")
-    public ApiResponse<ArticleResponse> editArticle(@PathVariable Long articleId) {
-        return ApiResponse.found(null);
     }
 
     // TODO: 2023/09/13 뉴스 기사 삭제 API
