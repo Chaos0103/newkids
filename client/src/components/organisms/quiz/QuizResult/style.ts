400c3c8aecd264fb6c721177248364a78dc87744
jochanik <whcksdlr5909@naver.com>
2023-09-19 09:24:50 +0900

    design: 주간 어휘 퀴즈 UI 디자인 변경
    주간 어휘 퀴즈 디자인 변경
    
    Closes #S09P22C205-219

---
+++ client/src/components/organisms/quiz/QuizResult/style.ts
@@ -0,0 +1,54 @@
+import styled from 'styled-components';
+
+export const QuizResultWrapper = styled.div`
+	margin-top: 3rem;
+
+	.score-wrapper {
+		margin-top: 3rem;
+		display: flex;
+		justify-content: center;
+		align-item: center;
+	}
+
+	.score {
+		color: var(--sub-color-1);
+		font-size: 2rem;
+	}
+
+	.sub-title-wrapper {
+		margin-top: 3rem;
+		display: flex;
+		justify-content: center;
+		align-item: center;
+	}
+
+	.sub-title {
+		font-size: 2rem;
+	}
+
+	.answer-wrapper {
+		margin-top: 3rem;
+		display: flex;
+		justify-content: center;
+		align-item: center;
+	}
+
+	.answer {
+		font-size: 1.5rem;
+		color: var(--sub-color-1);
+	}
+
+	.exp-wrapper {
+		margin-top: 3rem;
+		display: flex;
+		justify-content: center;
+		align-item: center;
+	}
+
+	.quiz-button {
+		display: flex;
+		justify-content: center;
+		align-item: center;
+		margin-top: 3rem;
+	}
+`;
--- client/src/layouts/page/QuizPageLayout/style.ts
+++ client/src/layouts/page/QuizPageLayout/style.ts
@@ -1,10 +1,3 @@
 import styled from 'styled-components';
 
-export const QuizPageLayoutContainer = styled.div`
-	.quiz-button {
-		display: flex;
-		justify-content: center;
-		align-item: center;
-		margin-top: 3rem;
-	}
-`;
+export const QuizPageLayoutContainer = styled.div``;
