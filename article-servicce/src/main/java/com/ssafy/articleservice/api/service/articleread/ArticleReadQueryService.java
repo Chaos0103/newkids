75becb33a66de81654838db9d03ee8c1f7bdce11
임우택 <lyt1228@naver.com>
2023-09-19 11:35:54 +0900

    feat(article-service): 읽은 뉴스 기사 조회 서비스 구현
    Closes S09P22C205-404

--- article-servicce/src/main/java/com/ssafy/articleservice/api/service/articleread/ArticleReadQueryService.java
+++ article-servicce/src/main/java/com/ssafy/articleservice/api/service/articleread/ArticleReadQueryService.java
@@ -4,9 +4,12 @@ import com.ssafy.articleservice.api.controller.article.response.ArticleReadRespo
 import com.ssafy.articleservice.domain.articleread.repository.ArticleReadQueryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -16,6 +19,8 @@ public class ArticleReadQueryService {
     private final ArticleReadQueryRepository articleReadQueryRepository;
 
     public Page<ArticleReadResponse> getMyArticleRead(String memberKey, Pageable pageable) {
-        return null;
+        List<ArticleReadResponse> content = articleReadQueryRepository.findByMemberKey(memberKey, pageable);
+        long totalCount = articleReadQueryRepository.getTotalCount(memberKey);
+        return new PageImpl<>(content, pageable, totalCount);
     }
 }
