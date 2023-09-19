a810f0e3706977a3dfa284d2148717f242f0daa3
임우택 <lyt1228@naver.com>
2023-09-19 11:24:52 +0900

    feat(article-service): 읽은 뉴스 기사 조회 구현
    Closes S09P22C205-404

--- article-servicce/src/test/java/com/ssafy/articleservice/domain/articleread/repository/ArticleReadQueryRepositoryTest.java
+++ article-servicce/src/test/java/com/ssafy/articleservice/domain/articleread/repository/ArticleReadQueryRepositoryTest.java
@@ -2,12 +2,16 @@ package com.ssafy.articleservice.domain.articleread.repository;
 
 import com.ssafy.articleservice.IntegrationTestSupport;
 import com.ssafy.articleservice.api.controller.article.response.ArticleReadResponse;
+import com.ssafy.articleservice.domain.article.Article;
+import com.ssafy.articleservice.domain.article.repository.ArticleRepository;
 import com.ssafy.articleservice.domain.articleread.ArticleRead;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -21,26 +25,42 @@ class ArticleReadQueryRepositoryTest extends IntegrationTestSupport {
     @Autowired
     private ArticleReadRepository articleReadRepository;
 
+    @Autowired
+    private ArticleRepository articleRepository;
+
     @DisplayName("회원 고유키로 읽은 뉴스 기사를 조회한다.")
     @Test
     void findByMemberKey() {
         //given
         String memberKey = UUID.randomUUID().toString();
-        createArticleRead(memberKey);
-        createArticleRead(memberKey);
-        createArticleRead(memberKey);
+        Article article = createArticle();
+        createArticleRead(memberKey, article);
+        createArticleRead(memberKey, article);
+        createArticleRead(memberKey, article);
 
         //when
-        List<ArticleReadResponse> responses = articleReadQueryRepository.findByMemberKey(memberKey);
+        List<ArticleReadResponse> responses = articleReadQueryRepository.findByMemberKey(memberKey, PageRequest.of(0, 10));
 
         //then
         assertThat(responses).hasSize(3);
     }
 
-    private ArticleRead createArticleRead(String memberKey) {
+    private Article createArticle() {
+        Article article = Article.builder()
+            .title("전인혁 오늘 지각으로 댄스에 당첨...")
+            .subTitle("과연 오늘은 어떤 춤을?!")
+            .writer("임우택")
+            .publishedDate(LocalDateTime.of(2023, 9, 18, 9, 0))
+            .content("전인혁은 오늘 지각을 하였다. 아직 일어나지 않은 것 같다. 오늘의 춤을 선택해야지~")
+            .thumbnailImg("http://전즈리얼.jpg")
+            .build();
+        return articleRepository.save(article);
+    }
+
+    private ArticleRead createArticleRead(String memberKey, Article article) {
         ArticleRead articleRead = ArticleRead.builder()
             .memberKey(memberKey)
-            .article(null)
+            .article(article)
             .build();
         return articleReadRepository.save(articleRead);
     }
