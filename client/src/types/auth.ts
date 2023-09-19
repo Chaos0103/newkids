68a97f046195e77d7b9f9beb58b99cb164f552c5
wjs_5025@naver.com <wjs_5025@naver.com>
2023-09-19 17:53:46 +0900

    feat: set Store (member)

---
+++ client/src/types/auth.ts
@@ -0,0 +1,12 @@
+export interface IAuth {
+	memberkey: string;
+	token: string;
+}
+
+export interface IMember {
+	name: string;
+	age: number;
+	level: number;
+	exp: number;
+	nickname: string;
+}
