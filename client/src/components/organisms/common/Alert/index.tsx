174919dc76fe06663253246a83e860d12a0708ac
jochanik <whcksdlr5909@naver.com>
2023-09-19 18:03:16 +0900

    feat: alert창 공통 컴포넌트 구현
    alert창 공통 컴포넌트 구현
    
    Closes #S09P22C205-352

---
+++ client/src/components/organisms/common/Alert/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Swal from "sweetalert2";
+import { AlertContainer } from "./style";
+
+function Alert() {
+    return (
+        <AlertContainer>
+            경고창입니다.
+        </AlertContainer>
+    )
+}
+
+export default Alert;
