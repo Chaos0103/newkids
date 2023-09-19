56ccbef0f305a78eaa71b398a7f4ae3a9750411b
서용준 <seoyj505@naver.com>
2023-09-19 09:35:09 +0900

    feat: mypage 마크업
    mypage 페이지 레이아웃
    
    Closes#S09P22C205-257

---
+++ client/src/layouts/page/MyPageLayout/style.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const MyPageLayoutContainer = styled.div`
+	.user-info {
+		margin-top: 2rem;
+		display: flex;
+		height: 500px;
+		gap: 10px;
+	}
+`;
+
+// hr {
+// 	height: 3px;
+// 	border-style: dashed;
+// 	border-width: 3px 0 0 0;
+// 	color: var(--gray-200);
+// }
