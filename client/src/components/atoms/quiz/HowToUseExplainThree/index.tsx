24c90c2dfa22037c599c5a56b2c8911707ae43dc
jochanik <whcksdlr5909@naver.com>
2023-09-19 09:21:09 +0900

    feat: 주간 어휘 퀴즈 구현
    설명파트 atomic화
    
    Closes #S09P22C205-219

---
+++ client/src/components/atoms/quiz/HowToUseExplainThree/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { HowToUseExplainThreeWrapper } from './style';
+
+function HowToUseExplainThree() {
+	return (
+		<HowToUseExplainThreeWrapper>
+			3. 문제가 끝났으면 <b className="main-explain">단어장</b>에 단어를 넣을지 선택해주세요.
+			<br />
+			다음에 공부할 때 큰 무리가 없겠네요.
+		</HowToUseExplainThreeWrapper>
+	);
+}
+
+export default HowToUseExplainThree;
