cbb4882290c005564164c45bed721830e199730a
wjs_5025@naver.com <wjs_5025@naver.com>
2023-09-20 11:52:11 +0900

    refacotr: recoil 적용

--- client/src/router/PrivateRoute.tsx
+++ client/src/router/PrivateRoute.tsx
@@ -1,9 +1,10 @@
-import useMemberInfo from 'hooks/useMemberInfo';
 import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
+import { useRecoilState } from 'recoil';
+import { MemberInfoState } from 'store/auth';
 
 function PrivateRoute() {
-	const memberInfoState = useMemberInfo();
+	const [memberInfoState] = useRecoilState(MemberInfoState);
 
 	return memberInfoState ? <Outlet /> : <Navigate to="/auth/login" />;
 }
