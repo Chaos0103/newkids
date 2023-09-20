3e913013a8a2cc86ee8e61c8cd48bdb62cef68e4
서용준 <seoyj505@naver.com>
2023-09-20 11:12:18 +0900

    feat: MyAcitivity Page 구현
    활동 내역 페이지 구현
    
    Closes#S09P22C205-324

---
+++ client/src/layouts/page/MyActivityLayout/style.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const MyActivityLayoutContainer = styled.div`
+	.my-page-menu {
+		margin-top: 2rem;
+	}
+
+	.my-activity-chart {
+		margin-top: 1rem;
+	}
+`;
