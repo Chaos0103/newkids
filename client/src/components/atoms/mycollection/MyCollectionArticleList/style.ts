50caa3dccb30ef9bedc686a5d9d13d4f78258442
서용준 <seoyj505@naver.com>
2023-09-20 15:58:06 +0900

    feat: 모은 기사 컴포넌트 구현
    모은 기사 컴포넌트 구현
    
    Closes #S09P22C205-321

---
+++ client/src/components/atoms/mycollection/MyCollectionArticleList/style.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const MyCollectionArticleListWrapper = styled.div`
+	.collection-list-item {
+		margin-top: 1rem;
+		background-color: var(--main-color);
+		width: 100%;
+		height: 500px;
+	}
+`;
