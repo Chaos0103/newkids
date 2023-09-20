99257985b43d78658e41b5e2d7ebd6fe0100a32d
임우택 <lyt1228@naver.com>
2023-09-20 16:22:51 +0900

    test(keyword-service): 키워드 중복 조회 쿼리 테스트 구현
    Closes S09P22C205-405

---
+++ keyword-service/src/test/java/com/ssafy/keywordservice/domain/keyword/repository/KeywordRepositoryTest.java
@@ -0,0 +1,38 @@
+package com.ssafy.keywordservice.domain.keyword.repository;
+
+import com.ssafy.keywordservice.IntegrationTestSupport;
+import com.ssafy.keywordservice.domain.keyword.Keyword;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+
+class KeywordRepositoryTest extends IntegrationTestSupport {
+
+    @Autowired
+    private KeywordRepository keywordRepository;
+
+    @DisplayName("단어 존재 여부를 조회한다.")
+    @Test
+    void existByWord() {
+        //given
+        Keyword keyword = createdKeyword();
+
+        //when
+        Optional<Long> existWord = keywordRepository.existByWord("전인혁");
+
+        //then
+        assertThat(existWord).isPresent();
+    }
+
+    private Keyword createdKeyword() {
+        Keyword keyword = Keyword.builder()
+            .word("전인혁")
+            .build();
+        return keywordRepository.save(keyword);
+    }
+}
