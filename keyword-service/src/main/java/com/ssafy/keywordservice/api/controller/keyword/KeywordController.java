dbbdb8228fc68d56f95480602ed9103a3c4ac838
임우택 <lyt1228@naver.com>
2023-09-20 16:27:46 +0900

    feat(keyword-service): 키워드 등록 API 컨트롤러 구현
    Closes S09P22C205-405

--- keyword-service/src/main/java/com/ssafy/keywordservice/api/controller/keyword/KeywordController.java
+++ keyword-service/src/main/java/com/ssafy/keywordservice/api/controller/keyword/KeywordController.java
@@ -2,6 +2,7 @@ package com.ssafy.keywordservice.api.controller.keyword;
 
 import com.ssafy.keywordservice.api.controller.ApiResponse;
 import com.ssafy.keywordservice.api.controller.keyword.request.CreatedKeywordRequest;
+import com.ssafy.keywordservice.api.controller.keyword.response.KeywordResponse;
 import com.ssafy.keywordservice.api.service.keyword.KeywordService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,7 +19,13 @@ public class KeywordController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public ApiResponse<?> createKeyword(@RequestBody CreatedKeywordRequest request) {
-        return ApiResponse.created("돼지");
+    public ApiResponse<KeywordResponse> createKeyword(@RequestBody CreatedKeywordRequest request) {
+        log.debug("call KeywordController#createKeyword");
+        log.debug("request word={}", request.getWord());
+
+        KeywordResponse response = keywordService.createKeyword(request.getWord());
+        log.debug("response={}", response);
+
+        return ApiResponse.created(response);
     }
 }
