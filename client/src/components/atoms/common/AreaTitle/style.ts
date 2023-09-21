1ffb6c76e4bdef5d790df3684b114e8d0defde04
wjs_5025@naver.com <wjs_5025@naver.com>
2023-09-21 17:57:41 +0900

    feat: 공통 컴포넌트 구현 (AreaTitle, keywordListItem)

---
+++ client/src/components/atoms/common/AreaTitle/style.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const AreaTitleWrapper = styled.h2`
+	font-size: 1.5rem;
+	font-weight: bold;
+	padding: 1rem 0;
+`;
---
+++ client/src/components/atoms/common/KeywordListItem/style.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const KeywordListItemContainer = styled.div`
+	width: fit-content;
+	border: 2px solid var(--gray-400);
+	padding: 0.5rem 1rem;
+	border-radius: var(--radius-l);
+`;
