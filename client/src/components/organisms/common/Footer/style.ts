79bd536a6f00325278ad6c2aa8009e2d9af659c0
wjs_5025@naver.com <wjs_5025@naver.com>
2023-09-21 17:59:00 +0900

    feat: Footer 구현

---
+++ client/src/components/organisms/common/Footer/style.ts
@@ -0,0 +1,43 @@
+import styled from 'styled-components';
+
+export const FooterConatiner = styled.div`
+	display: flex;
+	justify-content: center;
+	height: 10rem;
+	background-color: var(--footer-bg-color);
+
+	.wrapper {
+		width: var(--content-width-l);
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: space-between;
+
+		.content {
+			display: flex;
+			flex-direction: column;
+			gap: 8px;
+
+			.links {
+				button {
+					padding: 0 10px 0 0;
+				}
+			}
+
+			.team {
+				margin-bottom: 1rem;
+			}
+
+			.copyright {
+				font-weight: bold;
+				display: flex;
+				align-items: center;
+			}
+		}
+
+		img {
+			width: 9rem;
+			height: 3rem;
+		}
+	}
+`;
