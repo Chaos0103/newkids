89f604b544671c0c86d1b1ab674dc4a0a7100d3e
jochanik <whcksdlr5909@naver.com>
2023-09-22 15:37:33 +0900

    refactor: 컴포넌트 alert창 구현
    axios를 활용해 alert창을 띄운 다음 페이지이동까지 확인 완료
    
    Closes #feature/S09P22C205-362

--- client/src/components/organisms/common/Alert/index.tsx
+++ client/src/components/organisms/common/Alert/index.tsx
@@ -1,22 +1,32 @@
-import React from 'react';
+import React, { Dispatch, SetStateAction } from 'react';
+import axios from 'axios';
 import Swal from 'sweetalert2';
 import Button from 'components/atoms/common/Button';
 import { AlertContainer } from './style';
 
 interface IAlertProps {
+	setStep: Dispatch<SetStateAction<number>>;
 	imageUrls: string;
 	imageHeights: number;
 	titles: string;
 	texts: string;
+	confirms: string;
+	colors: string;
 }
 
-function Alert({ imageUrls, imageHeights, titles, texts }: IAlertProps) {
+function Alert({ setStep, imageUrls, imageHeights, titles, texts, confirms, colors }: IAlertProps) {
 	const alert = () =>
-		Swal.fire({
-			imageUrl: imageUrls,
-			imageHeight: imageHeights,
-			title: titles,
-			text: texts,
+		axios.get(`//api.github.com/users/bandozen`).then(() => {
+			Swal.fire({
+				imageUrl: imageUrls,
+				imageHeight: imageHeights,
+				title: titles,
+				text: texts,
+				confirmButtonColor: colors,
+				confirmButtonText: confirms,
+			}).then(() => {
+				setStep(3);
+			});
 		});
 	return (
 		<AlertContainer>
