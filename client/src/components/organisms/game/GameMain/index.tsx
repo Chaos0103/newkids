a5c45ec719d9368afa072ebd7ff2932af340825a
jochanik <whcksdlr5909@naver.com>
2023-09-22 15:36:54 +0900

    style: 기존 alert창 삭제
    alert창이 잘 나오는지 확인하기 위한 alert창 코드 삭제
    
    Closes #feature/S09P22C205-362

--- client/src/components/organisms/game/GameMain/index.tsx
+++ client/src/components/organisms/game/GameMain/index.tsx
@@ -3,8 +3,6 @@ import Title from 'components/atoms/game/Title';
 import QuizImage from 'components/atoms/quiz/QuizImage';
 import Explain from 'components/atoms/game/Explain';
 import Button from 'components/atoms/common/Button';
-import Swal from 'sweetalert2';
-import 'animate.css';
 import { GameMainContainer } from './style';
 
 interface IGameMainProps {
@@ -24,23 +22,6 @@ function GameMain({ setStep }: IGameMainProps) {
 		}
 	};
 
-	const alertClick = () => {
-		if (!isDone) {
-			Swal.fire({
-				title: '이런! 단어장에 단어를 추가하세요.',
-				html: `단어장에 단어가 없어요. <br /> 기사를 읽고 단어를 추가하면 컨텐츠를 진행할 수 있어요.`,
-				confirmButtonText: '기사 보러가기',
-				confirmButtonColor: 'var(--main-color)',
-				showClass: {
-					popup: 'animate__animated animate__fadeInDown',
-				},
-				hideClass: {
-					popup: 'animate__animated animate__fadeOutUp',
-				},
-			});
-		}
-	};
-
 	useEffect(() => {
 		if (num === 0) {
 			setIsDone(false);
@@ -54,7 +35,6 @@ function GameMain({ setStep }: IGameMainProps) {
 			<Explain />
 			<div className="quiz-button">
 				<Button size="l" radius="m" color="Primary" text="시작하러 가기!" handleClick={handleClick} />
-				<Button size="l" radius="m" color="Primary" text="alert 테스트용입니다." handleClick={alertClick} />
 			</div>
 		</GameMainContainer>
 	);
