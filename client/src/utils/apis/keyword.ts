4de0613b0d030c2bb3976b01fb2d5017145ec37f
wjs_5025@naver.com <wjs_5025@naver.com>
2023-09-22 17:50:58 +0900

    rename: 메소드멍 변경

--- client/src/utils/apis/keyword.ts
+++ client/src/utils/apis/keyword.ts
@@ -1,7 +1,7 @@
 import { instance } from './instance';
 
 // 인기 키워드 Top 5 조회
-export const findPopularKeywordApi = async () => {
+export const getPopularKeywordApi = async () => {
 	const response = await instance.get('/keyword-service/popular');
 	return response;
 };
