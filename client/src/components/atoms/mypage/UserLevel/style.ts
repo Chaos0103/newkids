1f70b63bfba5dd13fca650e5a03a02ba084f1695
서용준 <seoyj505@naver.com>
2023-09-24 18:57:24 +0900

    feat: mypage 관련 컴포넌트(레벨)
    mypage 관련 컴포넌트 구현(레벨)
    
    Closes #S09P22C205-324-myactivity

--- client/src/components/atoms/mypage/UserLevel/style.ts
+++ client/src/components/atoms/mypage/UserLevel/style.ts
@@ -1,18 +1,29 @@
 import styled from 'styled-components';
 
 export const UserLevelWrapper = styled.div`
+	width: 100%;
+	margin-top: 1rem;
+
 	.user-level-wrapper {
 		display: flex;
 		gap: 20px;
 		justify-content: center;
-		background-color: var(--main-color);
+		align-items: end;
 	}
 
-	.user-level-image > div {
-		font-size: 36px;
+	.user-level-image {
+		width: 40px;
+		height: 40px;
 	}
 
-	.user-level-content > p {
-		font-size: 36px;
+	.level-title {
+		font-size: 30px;
+		color: var(--sub-color-1);
+		font-weight: bold;
+	}
+	.level-number {
+		font-size: 30px;
+		color: var(--main-color);
+		font-weight: bold;
 	}
 `;
