059b2be61544320e96a872a74167a8866e32def2
조찬익 <whcksdlr5909@gmail.com>
2023-09-25 02:58:25 +0900

    style: alert 코드 변경

--- client/src/components/organisms/common/Alert/index.tsx
+++ client/src/components/organisms/common/Alert/index.tsx
@@ -1,4 +1,4 @@
-import React, { Dispatch, SetStateAction } from 'react';
+import React, { Dispatch, SetStateAction, useState } from 'react';
 import axios from 'axios';
 import Swal from 'sweetalert2';
 import Button from 'components/atoms/common/Button';
@@ -9,23 +9,24 @@ interface IAlertProps {
 	imageUrls: string;
 	imageHeights: number;
 	titles: string;
-	texts: string;
 	confirms: string;
 	colors: string;
 }
 
-function Alert({ setStep, imageUrls, imageHeights, titles, texts, confirms, colors }: IAlertProps) {
+function Alert({ setStep, imageUrls, imageHeights, titles, confirms, colors }: IAlertProps) {
+	const [num, setNum] = useState(0);
+	console.log(num);
 	const alert = () =>
 		axios.get(`//api.github.com/users/bandozen`).then(() => {
 			Swal.fire({
 				imageUrl: imageUrls,
 				imageHeight: imageHeights,
 				title: titles,
-				text: texts,
 				confirmButtonColor: colors,
 				confirmButtonText: confirms,
 			}).then(() => {
-				setStep(3);
+				setNum(1);
+				setStep(2);
 			});
 		});
 	return (
