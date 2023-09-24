cd7e311585e0bd8f0fbb4ac630f143ec63f829e6
wjs_5025 <wjs_5025@naver.com>
2023-09-24 19:23:56 +0900

    feat: 간편사전 검색어 틀 구성

--- client/src/components/organisms/article/Dictionary/index.tsx
+++ client/src/components/organisms/article/Dictionary/index.tsx
@@ -3,16 +3,24 @@ import SearchBar from 'components/atoms/common/SearchBar';
 import { DictionaryContainer } from './style';
 
 function Dictionary() {
-	const [value, setValue] = useState('');
+	const [searchWord, setSearchWord] = useState('');
+
+	const search = async () => {
+		try {
+			alert(`${searchWord} 검색`);
+		} catch (error) {
+			console.error(error);
+		}
+	};
 
 	return (
 		<DictionaryContainer>
 			<h3>뜻을 모르는 단어가 있나요?</h3>
 			<SearchBar
-				value={value}
-				setValue={setValue}
+				value={searchWord}
+				setValue={setSearchWord}
 				size="full"
-				confirmSearch={() => {}}
+				confirmSearch={search}
 				placeholder="검색어를 입력하세요"
 				color="SubSecond"
 			/>
--- client/src/components/organisms/article/Dictionary/style.ts
+++ client/src/components/organisms/article/Dictionary/style.ts
@@ -8,4 +8,7 @@ export const DictionaryContainer = styled.div`
 	top: 10rem;
 	position: sticky;
 	padding: 1rem;
+	display: flex;
+	flex-direction: column;
+	gap: 1rem;
 `;
