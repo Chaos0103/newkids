85060b5398196174d347410a22415564edcbce1a
서용준 <seoyj505@naver.com>
2023-09-24 18:54:44 +0900

    feat: mypage 관련 컴포넌트(모은 기사)
    mypage 관련 컴포넌트 구현(모은 기사)
    
    Closes #S09P22C205-324-myactivity

--- client/src/components/atoms/myactivity/MyActivityArticleList/style.ts
+++ client/src/components/atoms/myactivity/MyActivityArticleList/style.ts
@@ -1,7 +1,20 @@
 import styled from 'styled-components';
 
 export const MyActivityArticleListWrapper = styled.div`
-	.my-activity-article-list-content {
-		background-color: var(--danger-color);
+	width: 100%;
+	height: 100%;
+
+	display: flex;
+	flex-wrap: wrap;
+	justify-content: space-between;
+	align-content: flex-start;
+
+	.article-list-text {
+		width: calc(25% - 10px);
+		margin-bottom: 10px;
+		img {
+			height: 150px;
+			widht: 250px;
+		}
 	}
 `;
