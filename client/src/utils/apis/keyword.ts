42ac359d83e79efde5e9004bfd693836689d9f16
wjs_5025 <wjs_5025@naver.com>
2023-09-24 18:09:15 +0900

    fix: api url 경로 변경

--- client/src/utils/apis/keyword.ts
+++ client/src/utils/apis/keyword.ts
@@ -2,6 +2,6 @@ import { instance } from './instance';
 
 // 인기 키워드 Top 5 조회
 export const getPopularKeywordApi = async () => {
-	const response = await instance.get('/keyword-service/popular');
+	const response = await instance.get('/keyword-service/api/popular');
 	return response;
 };
