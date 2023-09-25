3f4418e3223a599dcfa55a841e68cade3a77aac2
최영환 <longbright0804@gmail.com>
2023-09-25 11:57:30 +0900

    feat: 현재 사용자의 관심사 조회 쿼리 구현 - keyword_dao#get_member_keyword_by_member_key
    Closes #S09P22C205-487

--- recommendation-server/dao/keyword_dao.py
+++ recommendation-server/dao/keyword_dao.py
@@ -48,3 +48,38 @@ def get_member_keyword_all(age):
         connection.close()
 
     return row
+
+
+def get_member_keyword_by_member_key(member_key):
+    """
+    현재 회원의 관심사 조회
+
+    :param member_key: 현재 회원의 고유값
+    :return: 현재 회원의 관심사
+    """
+    row = None
+    connection = None
+
+    try:
+        connection = connection_pool.get_connection()
+        cursor = connection.cursor(dictionary=True)
+
+        sql = (
+            "SELECT m.member_key, k.keyword_id, k.word, m.age "
+            "FROM keyword_search as ks "
+            "JOIN keyword as k on ks.keyword_id = k.keyword_id "
+            "JOIN members as m on ks.member_key = m.member_key "
+            "WHERE m.member_key = %s "
+            "ORDER BY ks.count "
+            "LIMIT 5 "
+        )
+
+        cursor.execute(sql, (member_key, ))
+        row = cursor.fetchone()
+
+    except Exception as e:
+        print(f"Exception={e}")
+    finally:
+        connection.close()
+
+    return row
