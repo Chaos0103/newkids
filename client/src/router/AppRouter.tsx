d65f724fc38c54d8d834597488117d3e31457b26
wjs_5025@naver.com <wjs_5025@naver.com>
2023-09-25 09:27:04 +0900

    style: 문법 수정

--- client/src/router/AppRouter.tsx
+++ client/src/router/AppRouter.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 import { GlobalFonts } from 'styles/GlobalFonts';
 import { GlobalStyles } from 'styles/GlobalStyles';
@@ -37,12 +37,12 @@ function AppRouter() {
 							<Route path="/mypage/info" element={<MyPage />} />
 							<Route path="/mypage/activity" element={<MyActivity />} />
 
-						<Route path="/" element={<PrivateRoute />}>
-							<Route path="/game" element={<div />} />
-							<Route path="/quiz" element={<div />} />
-						</Route>
-					</Routes>
-				</BrowserRouter>
+							<Route path="/" element={<PrivateRoute />}>
+								<Route path="/game" element={<GamePage />} />
+								<Route path="/quiz" element={<QuizPage />} />
+							</Route>
+						</Routes>
+					</BrowserRouter>
 				</AuthProvider>
 			</ApplicationLayout>
 		</RecoilRoot>
