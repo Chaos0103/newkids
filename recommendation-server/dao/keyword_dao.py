fd007b0996fcca17b0f518f7c18a5b6196824fc0
최영환 <longbright0804@gmail.com>
2023-09-25 10:05:35 +0900

    feat: 현재 사용자와 같은 나이의 회원들이 관심을 가진 키워드 리스트 조회 쿼리 구현 - keyword_dao#get_member_keyword_all
    Closes #S09P22C205-487

---
+++ recommendation-server/dao/keyword_dao.py
@@ -0,0 +1,50 @@
+import mysql.connector.pooling
+
+# 커넥션 풀 생성
+connection_pool = mysql.connector.pooling.MySQLConnectionPool(
+    pool_name="my_pool",
+    pool_size=5,
+    host="127.0.0.1",
+    port=3306,
+    user="ssafy",
+    password="ssafy",
+    database="newkids",
+    charset="utf8"
+)
+
+
+def get_member_keyword_all(age):
+    """
+    현재 사용자와 같은 나이의 회원들이 관심을 가진 키워드 리스트 조회
+    조회 횟수가 높은 것을 관심도가 높은 것으로 가정
+    
+    :param age: 현재 사용자의 나이
+    :return: 현재 사용자와 같은 나이의 회원들이 관심을 가진 키워드 리스트
+    """
+
+    row = None
+    connection = None
+
+    try:
+        connection = connection_pool.get_connection()
+        cursor = connection.cursor(dictionary=True)
+
+        sql = (
+            "SELECT m.member_key, k.keyword_id, k.keyword_name, ks.count, m.age"
+            "FROM keyword_search as ks "
+            "JOIN keywords as k on ks.keyword_id = k.keyword_id "
+            "JOIN member as m on ks.member_key = m.member_key "
+            "WHERE m.age = %s "
+            "ORDER BY count "
+            "LIMIT 5 "
+        )
+
+        cursor.execute(sql, (age, ))
+        row = cursor.fetchall()
+
+    except Exception as e:
+        print(f"Exception={e}")
+    finally:
+        connection.close()
+
+    return row
