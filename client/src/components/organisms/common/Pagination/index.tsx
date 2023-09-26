0309bb53726a9449afff1846b2595e206c47a4cb
wjs_5025@naver.com <wjs_5025@naver.com>
2023-09-26 17:14:16 +0900

    feat: 페이지네이션 컴포넌트 구현

---
+++ client/src/components/organisms/common/Pagination/index.tsx
@@ -0,0 +1,88 @@
+import React, { Dispatch, SetStateAction, useEffect, useState } from 'react';
+import { PaginationContainer, PaginationItemWrapper } from './style';
+
+function PaginationItem({ page, isActive, handleClick }: { page: number; isActive: boolean; handleClick: () => void }) {
+	return (
+		<PaginationItemWrapper $isActive={isActive} onClick={handleClick}>
+			{page}
+		</PaginationItemWrapper>
+	);
+}
+
+interface IPaginationProps {
+	currentPage: number;
+	totalPages: number;
+	size: number;
+	setCurrentPage: Dispatch<SetStateAction<number>>;
+	currentGroup: number;
+	setCurrentGroup: Dispatch<SetStateAction<number>>;
+}
+
+function Pagination(props: IPaginationProps) {
+	const { currentPage, totalPages, size, setCurrentPage, currentGroup, setCurrentGroup } = props;
+	const [pageNumbers, setPageNumbers] = useState<number[]>([]);
+
+	const setNumbers = () => {
+		const newPage = [];
+		let endPage = currentGroup * size;
+
+		if (endPage > totalPages) {
+			endPage = totalPages;
+		}
+
+		for (let i = currentPage; i <= endPage; i += 1) {
+			newPage.push(i);
+		}
+
+		setPageNumbers(newPage);
+	};
+
+	useEffect(() => {
+		setNumbers();
+	}, [totalPages, currentGroup]);
+
+	return (
+		<PaginationContainer>
+			{currentGroup !== 1 ? (
+				<button
+					type="button"
+					onClick={() => {
+						setCurrentPage((currentGroup - 1) * 10 - 9);
+						setCurrentGroup(currentGroup - 1);
+					}}
+				>
+					{'< 이전'}
+				</button>
+			) : (
+				<div />
+			)}
+
+			{pageNumbers.map((el) => (
+				<PaginationItem
+					page={el}
+					key={el}
+					isActive={el === currentPage}
+					handleClick={() => {
+						setCurrentPage(el);
+					}}
+				/>
+			))}
+
+			{currentGroup !== totalPages / size ? (
+				<button
+					type="button"
+					onClick={() => {
+						setCurrentPage(currentGroup * 10 + 1);
+						setCurrentGroup(currentGroup + 1);
+					}}
+				>
+					{'다음 >'}
+				</button>
+			) : (
+				<div />
+			)}
+		</PaginationContainer>
+	);
+}
+
+export default Pagination;
