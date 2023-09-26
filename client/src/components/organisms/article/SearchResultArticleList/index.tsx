b01fef554fda965544cee0a6b868e49d5da355b2
wjs_5025@naver.com <wjs_5025@naver.com>
2023-09-26 17:15:24 +0900

    design: 기사 검색결과 디자인 수정

--- client/src/components/organisms/article/SearchResultArticleList/index.tsx
+++ client/src/components/organisms/article/SearchResultArticleList/index.tsx
@@ -5,14 +5,20 @@ import { SearchResultArticleListContainer } from './style';
 
 interface ISearchResultArticleListProps {
 	articles: IArticle[];
+	totalElements: number;
 }
 
 function SearchResultArticleList(props: ISearchResultArticleListProps) {
-	const { articles } = props;
+	const { articles, totalElements } = props;
 
 	return (
 		<SearchResultArticleListContainer>
-			{articles ? articles.map((el) => <SearchResultArticleListItem key={el.articleId} article={el} />) : <div />}
+			<h3>총 {totalElements}건의 기사 검색결과가 있습니다.</h3>
+			{articles.length ? (
+				articles.map((el) => <SearchResultArticleListItem key={el.articleId} article={el} />)
+			) : (
+				<div>기간 내 검색된 기사가 없습니다.</div>
+			)}
 		</SearchResultArticleListContainer>
 	);
 }
--- client/src/components/organisms/article/SearchResultArticleList/style.ts
+++ client/src/components/organisms/article/SearchResultArticleList/style.ts
@@ -4,4 +4,5 @@ export const SearchResultArticleListContainer = styled.div`
 	display: flex;
 	flex-direction: column;
 	gap: 1rem;
+	min-height: 60vh;
 `;
