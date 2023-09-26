9efedb52a91aaca2d8a3ca3c36e2b81776873562
wjs_5025@naver.com <wjs_5025@naver.com>
2023-09-26 17:15:05 +0900

    feat: API 호출틀 구현

--- client/src/components/organisms/article/RecommendedArticleList/index.tsx
+++ client/src/components/organisms/article/RecommendedArticleList/index.tsx
@@ -2,23 +2,23 @@ import React, { useState, useEffect } from 'react';
 import { IArticle } from 'types/article';
 import { DUMMY_ARTICLES_6 } from 'constants/dummy';
 import RectangleArticleListItem from 'components/atoms/article/RectangleArticleListItem';
-import { getAllArticleApi } from 'utils/apis/article';
+import { getAllRecommededPeerArticleApi } from 'utils/apis/article';
 import { RecommendedArticleListContainer } from './style';
 
 function RecommendedArticleList() {
 	const [articles, setArticles] = useState<IArticle[]>([]);
 
+	// TODO : 추천 API 나오면 교체
 	const fetchData = async () => {
 		try {
-			const response = await getAllArticleApi('2023-01-01', '2023-09-01');
+			const response = await getAllRecommededPeerArticleApi();
+			setArticles(DUMMY_ARTICLES_6);
 			console.log('::getAllArticleApi', response);
 		} catch (error) {
 			console.error(error);
 		}
 	};
-	// TODO : 추천 API 나오면 교체
 	useEffect(() => {
-		setArticles(DUMMY_ARTICLES_6);
 		fetchData();
 	}, []);
 
