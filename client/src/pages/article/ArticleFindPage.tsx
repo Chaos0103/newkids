715d1660f50c6418bf8f24c2a5dc4135ef3bf063
wjs_5025@naver.com <wjs_5025@naver.com>
2023-09-26 09:11:35 +0900

    feat: 기사검색페이지 구현

--- client/src/pages/article/ArticleFindPage.tsx
+++ client/src/pages/article/ArticleFindPage.tsx
@@ -1,15 +1,25 @@
+import React, { useState } from 'react';
 import Footer from 'components/organisms/common/Footer';
 import PageLayout from 'layouts/common/PageLayout';
 import ArticleFindPageLayout from 'layouts/page/ArticleFindPageLayout';
-import React from 'react';
+import SearchOptions from 'components/organisms/article/SearchOptions';
+import { IArticle } from 'types/article';
+import SearchResultArticleList from 'components/organisms/article/SearchResultArticleList';
 
 function ArticleFindPage() {
+	const [resultArticles, setResultArticles] = useState<IArticle[]>([]);
+
 	return (
 		<PageLayout>
 			<ArticleFindPageLayout
-				TrendingKeywordList={<div />}
-				SearchOptions={<div />}
-				ResultArticleList={<div />}
+				SearchOptions={
+					<>
+						<h3>기간별 조회</h3>
+						<SearchOptions setResultArticles={setResultArticles} />
+					</>
+				}
+				ResultArticleList={<SearchResultArticleList articles={resultArticles} />}
+				Pagination={<div />}
 				Footer={<Footer />}
 			/>
 		</PageLayout>
