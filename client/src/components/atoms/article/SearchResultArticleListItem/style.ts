2b34703dc6439af09a760b78ddcfa6c8ad2d9eac
wjs_5025@naver.com <wjs_5025@naver.com>
2023-09-26 09:06:41 +0900

    feat: 검색 결과 리스트 아이템 구현

---
+++ client/src/components/atoms/article/SearchResultArticleListItem/style.ts
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+
+export const SearchResultArticleListItemContainer = styled.div`
+	border-radius: var(--radius-m);
+	display: flex;
+	flex-direction: row;
+	gap: 1rem;
+	color: var(--gray-500);
+	transition: all 0.1s;
+
+	&:hover {
+		background-color: var(--gray-200);
+		cursor: pointer;
+		transform: scale(1.05);
+	}
+
+	img {
+		border-radius: var(--radius-m);
+		width: 200px;
+		height: 150px;
+	}
+
+	.content {
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		justify-content: space-around;
+
+		h3 {
+			font-size: 1.3rem;
+			font-weight: bold;
+			white-space: normal;
+			display: -webkit-box;
+			-webkit-line-clamp: 2;
+			-webkit-box-orient: vertical;
+			overflow: hidden;
+		}
+
+		.article-info {
+		}
+	}
+`;
