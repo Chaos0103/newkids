c0d0e37fd988f328fca3196bb3ed3cdb8dddacbe
wjs_5025@naver.com <wjs_5025@naver.com>
2023-09-26 17:04:42 +0900

    refactor: 불필요한 api 함수 제거

--- client/src/utils/apis/vocabulary.ts
+++ client/src/utils/apis/vocabulary.ts
@@ -1,48 +1,35 @@
-import { PatchWordApiBody, RegistWordApiBody } from 'types/api';
+import { RegistrVocabularyApiBody } from 'types/api';
 import { instance } from './instance';
 
 // Word
-
-// 단어 등록
-export const registWordApi = async (body: RegistWordApiBody) => {
-	const response = await instance.post('/words', body);
-	return response;
-};
-
-// 단어 조회 (간편 사전 검색)
-export const findWordByKeywordApi = async (keyword: string, pageNum: number) => {
-	const response = await instance.get(`/words?keyword=${keyword}&pageNum=${pageNum}`);
-	return response;
-};
-
-// 단어 수정
-export const patchWordApi = async (wordId: string, body: PatchWordApiBody) => {
-	const response = await instance.patch(`/words/${wordId}`, body);
-	return response;
-};
-
-// 단어 삭제
-export const deleteWordApi = async (wordId: string) => {
-	const response = await instance.delete(`/words/${wordId}`);
+// 국립국어원 단어 조회 (간편 사전 검색)
+export const getAllWordApi = async (keyword: string, pageNum?: number) => {
+	const response = await instance.get(`/vocabulary-service/api/words?${keyword}&pageNum=${pageNum ?? 1}`);
 	return response;
 };
 
 // Vocabulary
 
 // -- 단어장에 등록
-export const registVocabularyApi = async (wordId: string) => {
-	const response = await instance.post(`/vocabulary/${wordId}`);
+export const registVocabularyApi = async (memberKey: string, body: RegistrVocabularyApiBody) => {
+	const response = await instance.post(`/vocabulary-service/api/${memberKey}`, body);
+	return response;
+};
+
+// -- 단어장에 등록
+export const getAllVocabularyApi = async (memberKey: string) => {
+	const response = await instance.post(`/vocabulary-service/api/${memberKey}`);
 	return response;
 };
 
 // -- 단어장에 체크
 export const checkVocabularyApi = async (wordId: string) => {
-	const response = await instance.patch(`/vocabulary/${wordId}`);
+	const response = await instance.patch(`/vocabulary-service/api/${wordId}`);
 	return response;
 };
 
 // -- 단어장에서 삭제
 export const deleteVocabularyApi = async (wordId: string) => {
-	const response = await instance.delete(`/vocabulary/${wordId}`);
+	const response = await instance.delete(`/vocabulary-service/api/${wordId}`);
 	return response;
 };
