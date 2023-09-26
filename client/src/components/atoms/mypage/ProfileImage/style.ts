c6024dcff34e124f0d5f6c38548405866cc72d82
서용준 <seoyj505@naver.com>
2023-09-26 16:20:43 +0900

    design : Mypage css 수정
    Mypage Profile css 수정
    
    Closes #S09P22C205-452

--- client/src/components/atoms/mypage/ProfileImage/style.ts
+++ client/src/components/atoms/mypage/ProfileImage/style.ts
@@ -1,10 +1,13 @@
 import styled from 'styled-components';
 
 export const ProfileImageWrapper = styled.div`
-	color: var(--danger-color);
-	img {
-		width: 40px;
-		height: 40px;
-		border-radius: var(--radius-l);
+	height: 100%;
+	.profile-image {
+		height: 100%;
+		img {
+			width: 75px;
+			height: 100%;
+			border-radius: var(--radius-l);
+		}
 	}
 `;
