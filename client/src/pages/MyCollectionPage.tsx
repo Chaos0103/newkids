684b6fd754c1e52933cf20df30c5f977e8a8d509
서용준 <seoyj505@naver.com>
2023-09-27 11:23:44 +0900

    rename : 파일명 수정
    기존 파일명 + Page로 수정
    
    Closes #S09P22C205-453

--- client/src/pages/MyCollectionPage.tsx
+++ client/src/pages/MyCollectionPage.tsx
@@ -1,20 +1,15 @@
 import MyPageMenu from 'components/organisms/common/MyPageMenu';
 import MyCollectionArticle from 'components/organisms/mycollection/MyCollectionArticle';
-import UserProfile from 'components/organisms/mypage/UserProfile';
 import PageLayout from 'layouts/common/PageLayout';
 import MyCollectionLayout from 'layouts/page/MyCollectionLayout';
 import React from 'react';
 
-function MyCollection() {
+function MyCollectionPage() {
 	return (
 		<PageLayout>
-			<MyCollectionLayout
-				MyPageMenu={<MyPageMenu />}
-				UserProfile={<UserProfile />}
-				MyCollectionArticle={<MyCollectionArticle />}
-			/>
+			<MyCollectionLayout MyPageMenu={<MyPageMenu />} MyCollectionArticle={<MyCollectionArticle />} />
 		</PageLayout>
 	);
 }
 
-export default MyCollection;
+export default MyCollectionPage;
--- client/src/pages/MyPage.tsx
+++ client/src/pages/MyPage.tsx
@@ -3,18 +3,12 @@ import PageLayout from 'layouts/common/PageLayout';
 import MyPageLayout from 'layouts/page/MyPageLayout';
 import UserDetail from 'components/organisms/mypage/UserDetail';
 import MyPageMenu from 'components/organisms/common/MyPageMenu';
-import UserProfile from 'components/organisms/mypage/UserProfile';
 import UserFigures from 'components/organisms/mypage/UserFigures';
 
 function MyPage() {
 	return (
 		<PageLayout>
-			<MyPageLayout
-				UserProfile={<UserProfile />}
-				MyPageMenu={<MyPageMenu />}
-				UserFigures={<UserFigures />}
-				UserDetail={<UserDetail />}
-			/>
+			<MyPageLayout MyPageMenu={<MyPageMenu />} UserFigures={<UserFigures />} UserDetail={<UserDetail />} />
 		</PageLayout>
 	);
 }
