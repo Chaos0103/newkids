4c7062a92752f3410405223f6f8354c9c5c64cdd
wjs_5025@naver.com <wjs_5025@naver.com>
2023-10-04 11:28:14 +0900

    feat: add type

--- client/src/types/keyword.ts
+++ client/src/types/keyword.ts
@@ -1,7 +1,16 @@
 export interface IKeyword {
 	keywordId: number;
 	word: string;
+}
+
+export interface IWordCloudKeyword {
+	keywordId: number;
+	text: string;
 	value: number;
+}
+
+export interface IWordCloudResponse extends IKeyword {
+	totalCount: number;
 }
 
 export interface IWord {
