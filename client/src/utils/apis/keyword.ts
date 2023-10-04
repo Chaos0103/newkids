7cce3514b82b1bdd4d7c2e918062bede528a4309
서용준 <seoyj505@naver.com>
2023-10-04 14:27:32 +0900

    feat : keyword api 기능 추가(나의 키워드 5개)
    나의 키워드 Top 5 기능 추가
    
    Closes #S09P22C205-456

--- client/src/utils/apis/keyword.ts
+++ client/src/utils/apis/keyword.ts
@@ -5,3 +5,9 @@ export const getPopularKeywordApi = async () => {
 	const response = await instance.get('/keyword-service/api/popular');
 	return response;
 };
+
+// 나의 키워드 Top 5 조회
+export const getMyKeywordApi = async (memberKey: string) => {
+	const response = await instance.get(`/keyword-service/api/my/${memberKey}`);
+	return response;
+};
