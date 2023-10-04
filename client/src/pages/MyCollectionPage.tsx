4111dc82f8eef636b9687f906d8fc5a8ace1e0dd
서용준 <seoyj505@naver.com>
2023-10-04 14:23:55 +0900

    feat : 모음 기사 페이지 페이징 구현
    모음 기사 페이지 페이징 기능 구현
    
    Closes #S09P22C205-456

--- client/src/pages/MyCollectionPage.tsx
+++ client/src/pages/MyCollectionPage.tsx
@@ -1,13 +1,59 @@
 import MyPageMenu from 'components/organisms/common/MyPageMenu';
+import Pagination from 'components/organisms/common/Pagination';
 import MyCollectionArticle from 'components/organisms/mycollection/MyCollectionArticle';
+// import { DUMMY_ARTICLES } from 'constants/dummyreadarticle';
 import PageLayout from 'layouts/common/PageLayout';
 import MyCollectionLayout from 'layouts/page/MyCollectionLayout';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { findAllReadArticleApi } from 'utils/apis/article';
 
 function MyCollectionPage() {
+	// 페이지네이션
+	const [currentPage, setCurrentPage] = useState(1);
+	const [totalPages, setTotalPages] = useState(3);
+	const [size, setSize] = useState(8);
+	const [currentGroup, setCurrentGroup] = useState(1);
+	const [articles, setArticles] = useState([]);
+	// const articles = DUMMY_ARTICLES;
+
+	const searchPage = async () => {
+		try {
+			const memberkey = localStorage.getItem('memberkey');
+			if (memberkey) {
+				const response = await findAllReadArticleApi(memberkey, currentPage);
+				console.log(response);
+				if (response.status === 200) {
+					setArticles(response.data.data.content);
+					setTotalPages(response.data.data.totalPages);
+					// setTotalPages(3);
+					setSize(response.data.data.size);
+				}
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	useEffect(() => {
+		searchPage();
+	}, [currentPage]);
+
 	return (
 		<PageLayout>
-			<MyCollectionLayout MyPageMenu={<MyPageMenu />} MyCollectionArticle={<MyCollectionArticle />} />
+			<MyCollectionLayout
+				MyPageMenu={<MyPageMenu />}
+				MyCollectionArticle={<MyCollectionArticle articles={articles} />}
+				Pagination={
+					<Pagination
+						currentPage={currentPage}
+						totalPages={totalPages}
+						setCurrentPage={setCurrentPage}
+						size={size}
+						currentGroup={currentGroup}
+						setCurrentGroup={setCurrentGroup}
+					/>
+				}
+			/>
 		</PageLayout>
 	);
 }
