043798ecfd4f8b9164ca0da7e35d34b9cd20d8e9
서용준 <seoyj505@naver.com>
2023-10-04 14:58:50 +0900

    fix : api  코드 오류 수정
    api 코드 오류 수정
    
    Closes #S09P22C205-456

--- client/src/utils/apis/keyword.ts
+++ client/src/utils/apis/keyword.ts
@@ -9,14 +9,10 @@ export const getPopularKeywordApi = async () => {
 // 나의 키워드 Top 5 조회
 export const getMyKeywordApi = async (memberKey: string) => {
 	const response = await instance.get(`/keyword-service/api/my/${memberKey}`);
-// 뉴스기사 키워드 조회
-
-export const getArticleKeywordApi = async (articleId: string) => {
-	const response = await instance.get(`/keyword-service/api/${articleId}/articles`);
 	return response;
 };
-// 뉴스기사 키워드 조회
 
+// 뉴스기사 키워드 조회
 export const getArticleKeywordApi = async (articleId: string) => {
 	const response = await instance.get(`/keyword-service/api/${articleId}/articles`);
 	return response;
