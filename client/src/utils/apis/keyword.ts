eca07248dbecd209f81c2d7e717e38a050186203
서용준 <seoyj505@naver.com>
2023-10-04 16:36:48 +0900

    Update keyword.ts

--- client/src/utils/apis/keyword.ts
+++ client/src/utils/apis/keyword.ts
@@ -6,13 +6,12 @@ export const getPopularKeywordApi = async () => {
 	return response;
 };
 
-<<<<<<< client/src/utils/apis/keyword.ts
 // 뉴스기사 키워드 조회
 export const getArticleKeywordApi = async (articleId: string) => {
 	const response = await instance.get(`/keyword-service/api/${articleId}/articles`);
 	return response;
 };
-=======
+
 // 나의 키워드 Top 5 조회
 export const getMyKeywordApi = async (memberKey: string) => {
 	const response = await instance.get(`/keyword-service/api/my/${memberKey}`);
