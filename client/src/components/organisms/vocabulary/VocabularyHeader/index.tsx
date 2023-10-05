21ef990b4c7f93ac51a705db7590b0eb74681080
서용준 <seoyj505@naver.com>
2023-10-05 16:35:03 +0900

    feat: Vocabulary Header 컴포넌트 변경
    vocabularybutton 컴포넌트 통합
    
    Closes #S09P22C205-553

--- client/src/components/organisms/vocabulary/VocabularyHeader/index.tsx
+++ client/src/components/organisms/vocabulary/VocabularyHeader/index.tsx
@@ -1,27 +1,71 @@
-import React, { useState } from 'react';
-import SearchBar from 'components/atoms/common/SearchBar';
-import VocabularyButton from 'components/atoms/vocabulary/VocabularyButton';
+import React, { Dispatch, SetStateAction, useEffect, useState } from 'react';
+import { getAllVocabularyApi, getCheckVocabularyApi } from 'utils/apis/vocabulary';
 import { VocabularyHeaderContainer } from './style';
 
 interface IVocabularyHeaderProps {
-	totalElements: number;
+	setIsChecked: Dispatch<SetStateAction<boolean>>;
 }
 
 function VocabularyHeader(props: IVocabularyHeaderProps) {
-	const { totalElements } = props;
-	const [value, setValue] = useState('');
+	const { setIsChecked } = props;
+	const [vocaCheckCnt, setVocaCheckCnt] = useState(0);
+	const memberkey = localStorage.getItem('memberkey');
+	const [allVoca, setAllVoca] = useState(0);
+	const handleKnowClick = () => {
+		setIsChecked(true);
+	};
+	const handleAllClick = () => {
+		setIsChecked(false);
+	};
+
+	const getCheckVocabulary = async () => {
+		try {
+			if (memberkey) {
+				const response = await getCheckVocabularyApi(memberkey);
+				if (response.status === 200) {
+					setVocaCheckCnt(response.data.data.checkedCount);
+				}
+			}
+		} catch (e) {
+			console.log(e);
+		}
+	};
+
+	const getAllVocabulary = async () => {
+		try {
+			if (memberkey) {
+				const response = await getAllVocabularyApi(memberkey, 1, false);
+				if (response.status === 200) {
+					setAllVoca(response.data.data.totalElements);
+				}
+			}
+		} catch (e) {
+			console.log(e);
+		}
+	};
+
+	useEffect(() => {
+		getCheckVocabulary();
+		getAllVocabulary();
+		window.addEventListener('checkVocabulary', getCheckVocabulary);
+		window.addEventListener('allVocabulary', getAllVocabulary);
+		return () => {
+			window.removeEventListener('checkVocabulary', getCheckVocabulary);
+			window.removeEventListener('allVocabulary', getAllVocabulary);
+		};
+	}, []);
+
 	return (
 		<VocabularyHeaderContainer>
-			<div className="vocabulary-header">
-				<VocabularyButton totalElements={totalElements} />
-				<SearchBar
-					size="s"
-					placeholder="검색어를 입력해주세요"
-					color="SubSecond"
-					value={value}
-					setValue={setValue}
-					confirmSearch={() => {}}
-				/>
+			<div className="voca-menu">
+				<button type="button" className="voca-total-button" onClick={handleAllClick}>
+					<p className="total-button-title">전체 단어 개수</p>
+					<p className="total-count">{allVoca}</p>
+				</button>
+				<button type="button" className="voca-know-button" onClick={handleKnowClick}>
+					<p className="know-button-title">아는 단어 개수</p>
+					<p className="know-count">{vocaCheckCnt}</p>
+				</button>
 			</div>
 		</VocabularyHeaderContainer>
 	);
