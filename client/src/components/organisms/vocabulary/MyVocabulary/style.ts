d3352d30ef7132d08483bbfd368f12cfe02646c6
서용준 <seoyj505@naver.com>
2023-10-05 16:33:36 +0900

    style : console 삭제
    console.log, 주석 삭제
    
    Closes #S09P22C205-553

--- client/src/components/organisms/vocabulary/MyVocabulary/style.ts
+++ client/src/components/organisms/vocabulary/MyVocabulary/style.ts
@@ -4,7 +4,6 @@ export const MyVocabularyContainer = styled.div`
 	.notebook-size {
 		margin-top: 3rem;
 		width: 100%;
-		height: 100%;
 		height: 700px;
 		border-radius: 50px;
 		border: 3px solid var(--gray-500);
@@ -12,9 +11,11 @@ export const MyVocabularyContainer = styled.div`
 		.voca-book {
 			margin-top: 7rem;
 			width: 100%;
+			height: 50%;
 			display: flex;
 			.left-page,
 			.right-page {
+				height: 100%;
 				width: 50%;
 				font-size: 20px;
 				font-weight: bold;
@@ -22,6 +23,9 @@ export const MyVocabularyContainer = styled.div`
 				.right-title {
 					display: flex;
 					justify-content: space-evenly;
+					> p {
+						font-size: 20px;
+					}
 				}
 				> hr {
 					margin-top: 1rem;
@@ -42,7 +46,7 @@ export const MyVocabularyContainer = styled.div`
 		background-color: rgba(255, 255, 255, 0);
 		border: 5.5px solid var(--gray-200);
 		border-radius: 50%;
-		border-bottom: 2.5px solid transparent; /* 수정된 부분: border-bottom을 조절하여 부드러운 끝 모양 생성 */
+		border-bottom: 2.5px solid transparent;
 		border-left: 2.5px solid transparent;
 		z-index: 1;
 	}
@@ -57,7 +61,7 @@ export const MyVocabularyContainer = styled.div`
 		background-color: rgba(255, 255, 255, 0);
 		border: 8px solid var(--gray-500);
 		border-radius: 50%;
-		border-bottom: 2.5px solid transparent; /* 수정된 부분: border-bottom을 조절하여 부드러운 끝 모양 생성 */
+		border-bottom: 2.5px solid transparent;
 		border-left: 2.5px solid transparent;
 	}
 
@@ -71,21 +75,21 @@ export const MyVocabularyContainer = styled.div`
 		background-color: rgba(255, 255, 255, 0);
 		border: 8px solid var(--gray-500);
 		border-radius: 65%;
-		border-bottom: 2.5px solid transparent; /* 수정된 부분: border-bottom을 조절하여 부드러운 끝 모양 생성 */
+		border-bottom: 2.5px solid transparent;
 		border-left: 2.5px solid transparent;
 	}
 
 	.small-ring {
 		position: absolute;
-		width: 15px; /* 작은 원의 너비 설정 */
-		height: 15px; /* 작은 원의 높이 설정 */
-		background-color: rgba(255, 255, 255, 0.5); /* 작은 원의 배경색 설정 */
+		width: 15px;
+		height: 15px;
+		background-color: rgba(255, 255, 255, 0.5);
 		border: 2.5px solid var(--gray-500);
-		border-radius: 50%; /* 작은 원을 원 모양으로 만듭니다. */
-		bottom: -3px; /* 큰 원 아래에 위치하도록 설정 */
-		left: 60%; /* 가운데 정렬 */
+		border-radius: 50%;
+		bottom: -3px;
+		left: 60%;
 		z-index: 1;
-		transform: translateX(0%); /* 가운데 정렬 */
+		transform: translateX(0%);
 	}
 
 	.vertical-line {
@@ -93,23 +97,16 @@ export const MyVocabularyContainer = styled.div`
 		top: 10%;
 		left: 50%;
 		transform: translateX(-50%);
-		width: 1px; /* 점선의 두께 조절 가능 */
+		width: 1px;
 		height: 80%;
-		border-left: 1px dashed var(--gray-300); /* 점선 스타일 및 색상 조절 가능 */
+		border-left: 1px dashed var(--gray-300);
 		border-right: none;
 	}
 	.word-list {
 		margin-top: 3rem;
-		height: 70%;
+		height: 100%;
 		display: flex;
 		flex-direction: column;
-		// justify-content: space-evenly;
 		gap: 65px;
-	}
-	.sort-button {
-		position: absolute;
-		top: 7%;
-		width: 90%;
-		text-align: right;
 	}
 `;
